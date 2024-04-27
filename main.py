9fd32782052f129243993cbff3f9f5293d583dd7
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-04-27 16:43:16 +0500

    Replace Cafe Route has been added with some minor fixes

--- main.py
+++ main.py
@@ -111,19 +111,19 @@ def add_cafe():
     if request.method == "POST":
         try:
             new_cafe = Cafe(
-                name=request.form['name'],
-                location=request.form['location'],
-                seats=request.form['seats'],
-                img_url=request.form['img_url'],
-                map_url=request.form['map_url'],
-                coffee_price=request.form['coffee_price'],
-                has_wifi=bool(request.form['has_wifi']),
-                has_toilet=bool(request.form['has_toilet']),
-                has_sockets=bool(request.form['has_sockets']),
-                can_take_calls=bool(request.form['can_take_calls']),
+                name=request.form["name"],
+                location=request.form["location"],
+                seats=request.form["seats"],
+                img_url=request.form["img_url"],
+                map_url=request.form["map_url"],
+                coffee_price=request.form["coffee_price"],
+                has_wifi=bool(request.form["has_wifi"]),
+                has_toilet=bool(request.form["has_toilet"]),
+                has_sockets=bool(request.form["has_sockets"]),
+                can_take_calls=bool(request.form["can_take_calls"]),
             )
-        except KeyError as err:
-            return jsonify(error={"Bad Request": str(err)}), 400
+        except KeyError:
+            return jsonify(error={"Bad Request": "One or all the keys are missing or invalid."}), 400
         else:
             try:
                 with app.app_context():
@@ -138,7 +138,7 @@ def add_cafe():
 
 # HTTP PUT/PATCH - Update Record
 # Price Update Route
-@app.route("/update-price/<cafe_id>", methods=["PATCH"])
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
 def update_price(cafe_id):
     # Request Validation
     if request.method == "PATCH":
@@ -160,6 +160,55 @@ def update_price(cafe_id):
         return jsonify(error={"Method Error": "Request Method Not Allowed."}), 405
 
 
+# Cafe Replace Route
+@app.route("/replace-cafe/<int:cafe_id>", methods=["PUT"])
+def replace_cafe(cafe_id):
+    # Request Validation
+    if request.method == "PUT":
+        # Counting in DB ids
+        total_ids = db.session.get(Cafe).count()
+        # Checking if the entered ID is in DB
+        if int(cafe_id) > total_ids or int(Cafe) < 1 or not db.session.query(db.exists().where(Cafe.id == cafe_id)).scalar():
+            return jsonify(error={"Not Found": f"Sorry! The Cafe with {cafe_id} ID was not found in database So, it cannot be replaced."}), 404
+        
+        # Getting the API Key from file
+        try:
+            api_file = open(file="API/api_key.txt")
+        except FileNotFoundError:
+            return jsonify(error={"Internal Error": "Sorry! API key file is not found in database."})
+        else:
+            api_key = api_file.read()
+            api_file.close()
+            
+        # Validating the API key entered by user
+        if api_key == request.args.get("api-key") or api_key == request.headers["api-key"]:
+            # Updating the DB
+            try:
+                target_cafe = db.session.get(Cafe).get(cafe_id)
+            except NoResultFound:
+                return jsonify(error={"Not Found": f"Sorry! The cafe with ID {cafe_id} was not found in the database."}), 404
+            else:
+                # Taking the args and replacing with the previous data in DB
+                try:
+                    target_cafe.name = request.args.get("name")
+                    target_cafe.location = request.args.get("location")
+                    target_cafe.seats = request.args.get("seats")
+                    target_cafe.img_url = request.args.get("img_url")
+                    target_cafe.map_url = request.args.get("map_url")
+                    target_cafe.coffee_price = request.args.get("coffee_price")
+                    target_cafe.has_wifi = request.args.get("has_wifi")
+                    target_cafe.has_toilet = request.args.get("has_toilet")
+                    target_cafe.has_sockets = request.args.get("has_sockets")
+                    target_cafe.can_take_calls = request.args.get("can_take_calls")
+                except KeyError:
+                    return jsonify(error={"Bad Request": "One or all the keys are missing or invalid."}), 400
+                else:
+                    db.session.commit()
+                    return jsonify(success={"Success": "Cafe replaced successfully."}), 200
+    else:
+        return jsonify(error={"Method Error": "Request Method Not Allowed."}), 405
+
+
 # HTTP DELETE - Delete Record
 @app.route("/delete-cafe/<int:cafe_id>", methods=["DELETE"])
 def delete_cafe(cafe_id):
@@ -168,10 +217,9 @@ def delete_cafe(cafe_id):
         # Counting the DB ids
         total_ids = db.session.query(Cafe).count()
         # Checking if the entered ID greater than DB ids
-        if (int(cafe_id) > total_ids or int(cafe_id) < 1 or
-                not db.session.query(db.exists().where(Cafe.id == cafe_id)).scalar()):
-            return jsonify(error={"Not Found": f"Sorry! The Cafe with {cafe_id} ID was not found in database So, "
-                                               f"it cannot be deleted."}), 404
+        if int(cafe_id) > total_ids or int(cafe_id) < 1 or not db.session.query(db.exists().where(Cafe.id == cafe_id)).scalar():
+            return jsonify(error={"Not Found": f"Sorry! The Cafe with {cafe_id} ID was not found in database So, it cannot be deleted."}), 404
+        
         # Getting the API key from the file
         try:
             api_file = open(file="API/api_key.txt", mode='r')
@@ -183,12 +231,11 @@ def delete_cafe(cafe_id):
 
         # Validating the API Key entered by user
         if api_key == request.args.get("api-key") or api_key == request.headers["api-key"]:
-            # Updating in DB
+            # Updating the DB
             try:
                 target_cafe = db.session.query(Cafe).get(cafe_id)
             except NoResultFound:
-                return jsonify(error={"Not Found": f"Sorry! The cafe with ID {cafe_id} was not found in the "
-                                                   f"database."}), 404
+                return jsonify(error={"Not Found": f"Sorry! The cafe with ID {cafe_id} was not found in the database."}), 404
             else:
                 db.session.delete(target_cafe)
                 db.session.commit()
